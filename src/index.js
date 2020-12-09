7a504a57aaf2d0cd78c7e772c768532b9f4da947
Snehil Saxena <snehilsaxena077@gmail.com>
2020-12-09 19:26:36 +0530

    Basic setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
+import Header from './components/Header/Header';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header />
   </React.StrictMode>,
   document.getElementById('root')
 );
